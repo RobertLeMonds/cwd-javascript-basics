b4eca5b2e55d0314cf5d62c1c496f56e7c2713de
Robert Le Monds <robertlemondsortiz@me.com>
2018-03-30 00:19:13 -0500

    working on javascript exercise prompt, console.log

---
+++ stalker.js
@@ -0,0 +1,11 @@
+var userFirst = prompt("What is your first name?");
+
+var userLast = prompt("What is your Last Name?");
+
+var userAge = prompt("What is your age?");
+
+console.log("I know who you are, " + userFirst);
+
+console.log("I also know your last name.... " + userLast);
+
+console.log("BUT I KNOW YOUR AGE TOOOOO " + userAge);
