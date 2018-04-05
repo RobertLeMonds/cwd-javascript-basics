ef04f9deeb497a1b454cf72ff5956ee365afcbd5
Robert Le Monds <robertlemondsortiz@me.com>
2018-04-05 14:55:49 -0500

    working on new exercise javascript conditionals

---
+++ operators.js
@@ -0,0 +1,11 @@
+if (age < 18) {
+	console.log("sorry, you are not old enough to enter the venue");
+}
+
+else if (age < 21) {
+	console.log("You can enter, but cannot drink");
+}
+
+else {
+	console.log("Come on in. You can drink.");
+}
