ebce2f27c40aa26992eef94a885c96d7081abdfa
Robert Le Monds <robertlemondsortiz@me.com>
2018-04-05 15:01:45 -0500

    adding exercise one by one

--- operators.js
+++ operators.js
@@ -1,3 +1,8 @@
+var age = prompt("How old are you?");
+
+
+
+
 if (age < 18) {
 	console.log("sorry, you are not old enough to enter the venue");
 }
@@ -6,6 +11,15 @@ else if (age < 21) {
 	console.log("You can enter, but cannot drink");
 }
 
+else if (age < 1) {
+	console.log("Error");
+}
+
+else if (age === 21) {
+	console.log("Happy 21st birthday!!")
+}
+
 else {
 	console.log("Come on in. You can drink.");
 }
+
